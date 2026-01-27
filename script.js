d668556a1bbe2dc316c549e0ad99162ffc05c8c4
rahulaes17 <rahulaes1704@gmail.com>
2026-01-27 22:58:48 +0530

    Footer

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ const observer = new IntersectionObserver(entries => {
 });
 
 cards.forEach(card => observer.observe(card));
+
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 400) {
+        scrollBtn.classList.add("show");
+    } else {
+        scrollBtn.classList.remove("show");
+    }
+});
+
+scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
